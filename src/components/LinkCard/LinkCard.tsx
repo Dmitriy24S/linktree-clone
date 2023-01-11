b8c72a908bb074184e22a81ee9b09372f170ce8f
Dmitriy24S <devilins24@gmail.com>
2023-01-12 01:31:30 +0200

    feat: LinkCard: show img
    * show twitter avatar in example twitter link

--- src/components/LinkCard/LinkCard.tsx
+++ src/components/LinkCard/LinkCard.tsx
@@ -1,19 +1,24 @@
+import Image from 'next/image'
 import React from 'react'
 
 interface Props {
   url: string
   title: string
+  image: string
 }
 
-const LinkCard = ({ url, title }: Props) => {
+const LinkCard = ({ url, title, image }: Props) => {
   return (
     <a
       href={url}
       target='_blank'
       rel='noopener noreferrer'
-      className='min-h-[40px] block w-full  rounded-md overflow-hidden bg-white px-4 py-2 text-gray-700 hover:scale-105 transition-all text-center font-semibold will-change-transform'
+      className='min-h-[40px] flex items-center w-full rounded-md overflow-hidden bg-white px-2 py-3 text-gray-700 hover:scale-105 transition-all text-center font-semibold will-change-transform'
     >
-      {title}
+      <div className='w-10 h-10 rounded-md overflow-hidden'>
+        {image && <Image src={image} alt={title} width={40} height={40} />}
+      </div>
+      <div className='w-full -ml-10'>{title}</div>
     </a>
   )
 }
